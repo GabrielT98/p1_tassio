9b9d7e54363460d9eb21dd39f1cb6133c7cf4148
Gabriel Tamiozzo <ra202011267@universidadedevassouras.edu.br>
2022-04-07 20:30:59 -0300

    adicionamento comentários na página de cada restaurante

--- prova/views.py
+++ prova/views.py
@@ -2,15 +2,15 @@
 from django.shortcuts import render
 from prova.entity import Restaurante,Especialista
 
-especialista1 = Especialista("GAbriel","melhor restaurante da cidade")
+especialista1 = Especialista("Gabriel","melhor restaurante da cidade")
 especialista2 =  Especialista("Lucas","Barato e bom")
 especialista3 = Especialista("Thiago","Comida diretamento do Líbano")
 
 restaurante1= Restaurante(1,"Varandas","Rua Jesus é o poder,Número 30, Centro, Vassouras","https://brasillocais.com/photo/106051.jpg",
-                          "10:00 - 18:00",especialista1)
+                          "10:00 - 18:00",[especialista1,especialista3])
 
-restaurante2 = Restaurante(2,"Sabor do Vale","Rua das Oliveiras,Número 40,Centro,VAssouras","https://media-cdn.tripadvisor.com/media/photo-s/12/99/81/69/fachada.jpg",
-                           "12:00 - 20:00",especialista2)
+restaurante2 = Restaurante(2,"Sabor do Vale","Rua das Oliveiras,Número 40,Centro,Vassouras","https://media-cdn.tripadvisor.com/media/photo-s/12/99/81/69/fachada.jpg",
+                           "12:00 - 20:00",[especialista2])
 
 
 restaurantes = [restaurante1,restaurante2]
